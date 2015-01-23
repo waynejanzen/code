6a5f98411a97bc4767b7cb18393fdd752ee65f66
Wayne <wayne@pixelgems.com>
2015-01-23 11:14:30 -0800

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,9 @@
 // JavaScript Document
 
+
+/****
+ * Create YouTube Tag
+ * **/
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
@@ -9,8 +13,13 @@ var divid = '';
 var player;
 
 
-
+/****
+ * Start Sliding Function
+ * **/
 $(function() {
+	/****
+	 * Set Variables
+	 * **/
 	var ul = $('.imgholder'),
 		li = ul.find('li'),
 		dots = $('.fs-circles li'),
@@ -21,7 +30,9 @@ $(function() {
 		sliding = false,
 		resizeslider = false;
 		
-		
+	/****
+	 * Start Video Function
+	 * **/	
 	var startVideo = function(id,width,height,div,c) {
 		divid = div
 		player = new YT.Player(div, {
@@ -32,6 +43,7 @@ $(function() {
 			events: {
 				'onStateChange': function(event) {
 					if (event.data == YT.PlayerState.ENDED) {
+						// when the video is done playing, change slide
 						console.log(c);
 						var n = c+1;
 						flip(n); 
@@ -41,6 +53,9 @@ $(function() {
 		});
 	}
 	
+	/****
+	 * Change Slide Function
+	 * **/
 	var flip = function(n) {
 		if(li.length > 0) {
 		sliding = true;
@@ -48,15 +63,19 @@ $(function() {
 		if(n < 0) n = li.length-1;
 		
 		
-		
+		// bring next slide forward and hide it
 		li.eq(n).hide().css({
 			'z-index':2
 		});
+		
+		// activate current slide dots
 		dots.removeClass('active');
 		dots.eq(n).addClass('active');
 		
+		// fade the next slide in
 		li.eq(n).fadeIn(transition,function() {
 			
+			// remove any video from the slider
 			$('.slidervideo').remove();
 			
 			li.eq(c).css({'z-index':0});
@@ -64,15 +83,17 @@ $(function() {
 			c = n;
 			sliding = false;
 			
+			// check if there's a video
 			if(li.eq(n).find('.vidholder').length > 0) {
 				var ve = li.eq(n).find('.vidholder');
 				ve.html('<div class="slidervideo" id="sv'+ve.attr('data-id')+'"></div>');
 				var w = ve.width();
 				var h = ve.height();
+				// play video
 				startVideo(ve.attr('data-ytid'),w,h,'sv'+ve.attr('data-id'),c);
 				
 			} else {
-			
+				// set Timeout to change the slide
 				flipper = setTimeout(function() { 
 					var n = c+1;
 					flip(n); 
@@ -89,7 +110,7 @@ $(function() {
 	}).show();
 	
 	
-	
+	// check if the first slide is a video
 	if(li.first().find('.vidholder').length > 0) {
 		var ve = li.first().find('.vidholder');
 		ve.html('<div class="slidervideo" id="sv'+ve.attr('data-id')+'"></div>');
@@ -106,15 +127,17 @@ $(function() {
 		}, delay);
 	}
 	
+	// right arrow event listener
 	$('a.fs-rightarrow').click(function() {
 		clearTimeout(flipper);
 		if(!sliding) flip(c+1);
 	});
-	
+	// left arrow event listener
 	$('a.fs-leftarrow').click(function() {
 		clearTimeout(flipper);
 		if(!sliding) flip(c-1);
 	});
+	// dot click event listener
 	$('.fs-circles a.circle').click(function() {
 		if(c != $(this).attr('data-id')) {
 			clearTimeout(flipper);
@@ -122,6 +145,7 @@ $(function() {
 		}
 	});
 	
+	// resize slider maintaining aspect ratio
 	var sliderresize = function() {
 		// dimensions of the slider
 		var sliderwidth = 1800;
